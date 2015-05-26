8f985ecbba05d492fa214d6f0d79d3bfba697759
plazma0 <aq132703@aquinas.ac.uk>
2015-05-26 21:51:35 +0100

    Skeleton (WIP)

---
+++ SampleName/SampleName/SampleName/Objects/Asset.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SampleName.Objects
+{
+    class Asset
+    {
+        private static string Name;
+        private static int DefaultValue;
+        private static int MortgageValue;
+        private static int Ranking; //1 is browns
+        private static int CostOfHouse;
+        private int NoOfHouses;
+        private int Rent;
+        public void Asset(string _Name, int _DefaultValue, int _MortgageValue, int _Ranking)
+        {
+            Name = _Name;
+            DefaultValue = _DefaultValue;
+            MortgageValue = _MortgageValue;
+            Ranking = _Ranking;
+        }
+
+        public int CalcHouseCost()
+        {
+            // Calculates the cost of each house for the property (Use DefaultValue & Ranking)
+        }
+
+        public int CalcRent()
+        {
+            // Calculates the cost of the current rent (Use Ranking & NoOfHouses & DefaultValue)
+        }
+
+        public void Sell()
+        {
+            // Sells the asset
+        }
+
+        public void Buy()
+        {
+            // Buys the asset
+        }
+    }
+}
---
+++ SampleName/SampleName/SampleName/Objects/Player.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SampleName
+{
+    class Player
+    {
+        private static int ID;
+        private int Balance;
+        private Objects.Asset[] Assets;
+        public Player(int _ID)
+        {
+            ID = _ID;
+        }
+    }
+}
